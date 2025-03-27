9d91ec56933d3c2b380a0d5a5972afe2ccb8815b
Arthur <arthur.barthelme78@gmail.com>
2025-03-27 23:02:39 +0100

    Enhance MessagingVendor enum with connection details for RabbitMQ and Redis

--- src/main/java/fr/milekat/utils/messaging/MessagingVendor.java
+++ src/main/java/fr/milekat/utils/messaging/MessagingVendor.java
@@ -1,6 +1,31 @@
 package fr.milekat.utils.messaging;
 
 public enum MessagingVendor {
-    RABBITMQ,
-    REDIS,
+    RABBITMQ("rabbitmq", "RabbitMQConnection",
+            "com.rabbitmq.client.Connection"),
+    REDIS("redis", "RedisConnection", "redis.clients.jedis.Jedis");
+
+    private final String messagingAdapter;
+    private final String adapterConnectionClass;
+    private final String driverClass;
+
+    MessagingVendor(String messagingAdapter, String adapterConnectionClass, String driverClass) {
+        this.messagingAdapter = messagingAdapter;
+        this.adapterConnectionClass = "fr.milekat.utils.messaging.adapter." +
+                messagingAdapter + ".connection." + adapterConnectionClass;
+        this.driverClass = driverClass;
+
+    }
+
+    public String getMessagingAdapter() {
+        return messagingAdapter;
+    }
+
+    public String getAdapterConnectionClass() {
+        return adapterConnectionClass;
+    }
+
+    public String getDriverClass() {
+        return driverClass;
+    }
 }
