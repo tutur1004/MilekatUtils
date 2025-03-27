326477928f20de39323fe43d11ec21c4947be2b2
Arthur <arthur.barthelme78@gmail.com>
2025-03-27 23:11:06 +0100

    Implement MessagingLoader with logging and messaging type handling

--- src/main/java/fr/milekat/utils/messaging/MessagingLoader.java
+++ src/main/java/fr/milekat/utils/messaging/MessagingLoader.java
@@ -1,4 +1,45 @@
 package fr.milekat.utils.messaging;
 
+import fr.milekat.utils.Configs;
+import fr.milekat.utils.MileLogger;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+
 public class MessagingLoader {
+    public static MileLogger messagingLogger = new MileLogger("MessagingLoader");
+    private final MessagingConnection loadedMessaging;
+
+    public MessagingLoader(@NotNull Configs config, @NotNull MileLogger logger) {
+        messagingLogger = logger;
+        String messagingType = config.getString("messaging.type");
+        messagingLogger.debug("Loading messaging type: " + messagingType);
+
+        Map<String, MessagingConnection> messagingAdapters = MessagingAdapterLoader.loadAdapters(config, logger);
+
+        switch (messagingType.toLowerCase()) {
+            case "rabbitmq":
+                loadedMessaging = messagingAdapters.get("rabbitmq");
+                break;
+            case "redis":
+                loadedMessaging = messagingAdapters.get("redis");
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported messaging type");
+        }
+
+        if (loadedMessaging.checkMessagingConnection()) {
+            messagingLogger.debug("Messaging loaded");
+        } else {
+            throw new IllegalArgumentException("Messaging is not loaded properly");
+        }
+    }
+
+    public MessagingConnection getLoadedMessaging() {
+        return loadedMessaging;
+    }
+
+    public static MileLogger getMessagingLogger() {
+        return messagingLogger;
+    }
 }
