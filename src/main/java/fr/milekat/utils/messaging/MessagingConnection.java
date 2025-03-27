58b8e672b23ce3e3c5bc59990529af91cf3fee05
Arthur <arthur.barthelme78@gmail.com>
2025-03-27 23:39:27 +0100

    Add RabbitMQConnection and RabbitMQConsumer classes for messaging integration

--- src/main/java/fr/milekat/utils/messaging/MessagingConnection.java
+++ src/main/java/fr/milekat/utils/messaging/MessagingConnection.java
@@ -1,6 +1,8 @@
 package fr.milekat.utils.messaging;
 
 import fr.milekat.utils.messaging.exceptions.MessagingLoadException;
+import fr.milekat.utils.messaging.exceptions.MessagingReceiveException;
+import fr.milekat.utils.messaging.exceptions.MessagingSendException;
 import org.jetbrains.annotations.Nullable;
 
 @SuppressWarnings("unused")
@@ -15,4 +17,8 @@ public interface MessagingConnection {
     default @Nullable String getPrefix() {
         return null;
     }
+
+    void sendMessage(String target, String message) throws MessagingSendException;
+
+    void registerMessageProcessor(Runnable messageProcessor) throws MessagingReceiveException;
 }
