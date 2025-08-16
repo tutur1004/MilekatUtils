a14b291bef5ab058e18344b1540ce67e0075b6c2
Arthur <arthur.barthelme78@gmail.com>
2025-08-16 14:24:08 +0200

    Add overloaded registerMessageProcessor method for enhanced message handling

--- src/main/java/fr/milekat/utils/messaging/MessagingConnection.java
+++ src/main/java/fr/milekat/utils/messaging/MessagingConnection.java
@@ -5,6 +5,8 @@ import fr.milekat.utils.messaging.exceptions.MessagingReceiveException;
 import fr.milekat.utils.messaging.exceptions.MessagingSendException;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.function.Consumer;
+
 @SuppressWarnings("unused")
 public interface MessagingConnection {
     /**
@@ -27,4 +29,5 @@ public interface MessagingConnection {
     void sendMessage(String target, String message) throws MessagingSendException;
 
     void registerMessageProcessor(Runnable messageProcessor) throws MessagingReceiveException;
+    void registerMessageProcessor(String processorName, Consumer<ReceivedMessage> messageHandler) throws MessagingReceiveException;
 }
