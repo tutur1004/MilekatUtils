715b5a7fe961fb42db9a1bf7f589166b922fb10d
Arthur <arthur.barthelme78@gmail.com>
2025-09-03 22:15:50 +0200

    Remove unused Receive Exception

--- src/main/java/fr/milekat/utils/messaging/exceptions/MessagingReceiveException.java
+++
@@ -1,7 +0,0 @@
-package fr.milekat.utils.messaging.exceptions;
-
-public class MessagingReceiveException extends RuntimeException {
-  public MessagingReceiveException(String message) {
-    super(message);
-  }
-}
