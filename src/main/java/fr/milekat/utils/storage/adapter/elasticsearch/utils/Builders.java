ecd2bd6d663f80d36dca34c93a2f164bfa337c3a
Arthur <arthur.barthelme78@gmail.com>
2025-10-30 00:39:29 +0100

    Refactor Builders to replace BoolQuery with TermQuery for simplified query construction

--- src/main/java/fr/milekat/utils/storage/adapter/elasticsearch/utils/Builders.java
+++ src/main/java/fr/milekat/utils/storage/adapter/elasticsearch/utils/Builders.java
@@ -1,29 +1,24 @@
 package fr.milekat.utils.storage.adapter.elasticsearch.utils;
 
-import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery;
+import co.elastic.clients.elasticsearch._types.query_dsl.TermQuery;
 import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings("unused")
 public class Builders {
     @NotNull
-    public static BoolQuery.Builder getBuilder(@NotNull String tagName, @NotNull Object tagValue) {
-        BoolQuery.Builder boolQuery = new BoolQuery.Builder();
-        if (tagValue instanceof String) {
-            String value = (String) tagValue;
-            boolQuery.must(mu -> mu.match(ma -> ma.field(tagName).query(value)));
-        } else if (tagValue instanceof Boolean) {
-            Boolean value = (Boolean) tagValue;
-            boolQuery.must(mu -> mu.match(ma -> ma.field(tagName).query(value)));
-        } else if (tagValue instanceof Integer) {
-            Integer value = (Integer) tagValue;
-            boolQuery.must(mu -> mu.match(ma -> ma.field(tagName).query(value)));
-        } else if (tagValue instanceof Long) {
-            Long value = (Long) tagValue;
-            boolQuery.must(mu -> mu.match(ma -> ma.field(tagName).query(value)));
-        } else if (tagValue instanceof Double) {
-            Double value = (Double) tagValue;
-            boolQuery.must(mu -> mu.match(ma -> ma.field(tagName).query(value)));
+    public static TermQuery.Builder getTermBuilder(@NotNull String tagName, @NotNull Object tagValue) {
+        TermQuery.Builder termQuery = new TermQuery.Builder();
+        if (tagValue instanceof String value) {
+            termQuery.field(tagName).value(value);
+        } else if (tagValue instanceof Boolean value) {
+            termQuery.field(tagName).value(value);
+        } else if (tagValue instanceof Integer value) {
+            termQuery.field(tagName).value(value);
+        } else if (tagValue instanceof Long value) {
+            termQuery.field(tagName).value(value);
+        } else if (tagValue instanceof Double value) {
+            termQuery.field(tagName).value(value);
         }
-        return boolQuery;
+        return termQuery;
     }
 }
